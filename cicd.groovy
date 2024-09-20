4f7a4b54951a168187a0efc421509d7b40fcea16
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:17 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/libuvport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/libuvport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libuvport.git'), string(name: 'PORT_DESCRIPTION', value: 'Cross-platform asynchronous I/O' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libuvport.git'), string(name: 'PORT_DESCRIPTION', value: 'Cross-platform asynchronous I/O' )]
   }
 }
